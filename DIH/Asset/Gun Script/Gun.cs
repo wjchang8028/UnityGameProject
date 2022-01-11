8ee1389448e7248719e8d6ba1b5da86bec7a3a45
eukjin <wjchang8028@naver.com>
2022-01-11 12:11:15 +0900

    Gun
    Gun

---
+++ DIH/Asset/Gun Script/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 10;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem MuzzleFlash;
+
+	void Update () {
+		
+        if(Input.GetButtonDown("Fire"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        MuzzleFlash.Play();
+
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
---
+++ DIH/Asset/Gun Script/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float range = 100f;
+    public int bulletsPerMag = 30;
+    public int buleetsLeft;
+
+    public Transform shootPoint;
+
+    public float fireRate = 0.1f;
+
+    float fireTimer;
+
+    void Start ()
+    {
+        
+    }
+
+    void Update ()
+    {
+        if(Input.GetButtonDown("Fire"))
+        {
+            Fire();
+        }
+
+        if (fireTimer < fireRate)
+            fireTimer += Time.deltaTime;
+    }
+
+    private void Fire()
+    {
+        if (fireTimer < fireRate) return;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(shootPoint.position, shootPoint.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name + " found! ");
+        }
+
+        fireTimer = 0.0f;
+    }
+}
